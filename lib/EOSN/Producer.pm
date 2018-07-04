bf1b8a19712d97654db517cc28d88921efe6204b
Matthew <matthew@mdarwin.ca>
2018-07-04 09:37:34 -0400

    add quotes around version numbers

--- lib/EOSN/Producer.pm
+++ lib/EOSN/Producer.pm
@@ -19,14 +19,14 @@ $content_types{png_jpg} = ['image/png', 'image/jpeg'];
 $content_types{svg} = ['image/svg+xml'];
 
 our %versions;
-$versions{db031363}{name} = 'mainnet-1.0.5';
-$versions{db031363}{current} = 0;
-$versions{aa351733}{name} = 'mainnet-1.0.6';
-$versions{aa351733}{current} = 0;
-$versions{b195012b}{name} = 'mainnet-1.0.7';
-$versions{b195012b}{current} = 1;
-$versions{36a043c5}{name} = 'mainnet-1.0.8';
-$versions{36a043c5}{current} = 1;
+$versions{'db031363'}{name} = 'mainnet-1.0.5';
+$versions{'db031363'}{current} = 0;
+$versions{'aa351733'}{name} = 'mainnet-1.0.6';
+$versions{'aa351733'}{current} = 0;
+$versions{'b195012b'}{name} = 'mainnet-1.0.7';
+$versions{'b195012b'}{current} = 1;
+$versions{'36a043c5'}{name} = 'mainnet-1.0.8';
+$versions{'36a043c5'}{current} = 1;
 
 # --------------------------------------------------------------------------
 # Class Methods
