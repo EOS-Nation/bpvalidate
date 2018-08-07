e4673dd3d8056ec92f5e5335c1733b456beedc77
Matthew <matthew@mdarwin.ca>
2018-08-06 23:43:11 -0400

    add prereq apt info

--- util/bpjson2chain.pl
+++ util/bpjson2chain.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl -w
 
+# apt install libjson-perl
 # Usage: bpjson2chain.pl producername < bp.json
 
+use strict;
 use JSON;
 
 my $producer = $ARGV[0] || "insertproducername";
@@ -11,6 +13,6 @@ while (<>) {
 	$data .= $_;
 }
 
-my $json = from_json($data);
+my $json = from_json($data) || die "$0: cannot parse JSON data";
 my $args = {owner => $producer, json => to_json($json)};
-print "cleos.sh push action producerjson set '" . to_json($args) . "' -p $producer@active\n";
+print "cleos.sh push action producerjson set '" . to_json($args) . "' -p $producer\@active\n";
