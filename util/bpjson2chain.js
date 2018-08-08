16116c22e5a46643ba59481c114b8403cd79a7b2
Denis <carriere.denis@gmail.com>
2018-08-08 15:27:02 -0400

    Create bpjson2chain.js

---
+++ util/bpjson2chain.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+const bpjsonFilePath = process.argv[2] || "bp.json";
+if (!bpjsonFilePath) new Error("bp.json filepath is required");
+
+const bpjson = JSON.parse(fs.readFileSync(bpjsonFilePath));
+const bpjsonString = JSON.stringify(JSON.stringify(bpjson));
+const owner = bpjson.producer_account_name
+
+console.log(`cleos push action producerjson set '{"owner": "${owner}", "json": ${bpjsonString}}' -p ${owner}@active`);
