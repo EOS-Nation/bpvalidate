dffbe613375446c74bf7df303c923a1f40f54de6
Matthew <matthew@mdarwin.ca>
2021-06-22 08:02:09 -0400

    change how to invoke new

--- lib/EOSN/UA.pm
+++ lib/EOSN/UA.pm
@@ -13,7 +13,7 @@ our @EXPORT_OK = qw(eosn_normal_ua eosn_cache_ua);
 # Subroutines
 
 sub eosn_normal_ua {
-	my $ua = new LWPx::ParanoidAgent;
+	my $ua = LWPx::ParanoidAgent->new;
 	#$ua->agent ('curl/7.64.0');
 	$ua->agent ('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0');
 	$ua->protocols_allowed (['http', 'https']);
@@ -23,7 +23,7 @@ sub eosn_normal_ua {
 }
 
 sub eosn_cache_ua {
-	my $ua = new EOSN::CachingAgent;
+	my $ua = EOSN::CachingAgent->new;
 	#$ua->agent ('curl/7.64.0');
 	$ua->agent ('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0');
 	$ua->protocols_allowed (['http', 'https']);
